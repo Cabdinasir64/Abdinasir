31fed9dd3038bbab553add91bd1ad33f2fd65acc
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-09 10:59:05 +0300

    fix: make user property in SessionData required instead of optional

--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -2,7 +2,7 @@ import 'express-session';
 
 declare module 'express-session' {
     interface SessionData {
-        user?: {
+        user: {
             userId: string;
             username: string;
             role: string;
