3b922d98973f3124768900940e1224b76b793942
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-09 10:47:37 +0300

    refactor: reorganize server setup and improve middleware order

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import dotenv from 'dotenv';
 import UserRoutes from './routes/userRoutes'
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT;
 const sessionSecret = process.env.SESSION_SECRET!
+
+const app = express();
+
+app.set('trust proxy', 1);
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
-
-app.use(express.json());
 
 app.use(session({
     name: 'portfolio.sid',
@@ -36,3 +41,5 @@ app.use('/api/user', UserRoutes);
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
