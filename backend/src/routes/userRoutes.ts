6436ef3fa84e755d3516648691e9eec2a11dbdd7
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-09 10:12:15 +0300

    feat: add login endpoint to user routes using Express router

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -4,5 +4,6 @@ import * as userControllers from '../controllers/userController';
 const router = Router();
 
 router.post('/', userControllers.registerUser);
+router.post('/login', userControllers.login);
 
 export default router;
