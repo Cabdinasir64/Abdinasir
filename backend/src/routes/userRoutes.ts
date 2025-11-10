64e60759ea4f60571962f136bbb57fa9c71cd2d5
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-10 15:02:20 +0300

    feat: add /me route with authentication and admin role check to fetch user profile

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import * as userControllers from '../controllers/userController';
 import { rateLimiter } from '../middleware/rateLimiter'
 import { authMiddleware } from '../middleware/authMiddleware';
+import { roleCheck } from '../middleware/roleCheck';
 
 const router = Router();
 
 router.post('/', userControllers.registerUser);
 router.post('/login', rateLimiter, userControllers.login);
 router.post('/logout', authMiddleware, userControllers.logout);
+router.get("/me", authMiddleware, roleCheck("admin"), userControllers.getMe);
 
 export default router;
