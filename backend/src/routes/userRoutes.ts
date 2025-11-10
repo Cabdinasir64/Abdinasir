fdaf8f3c82e67bd780db560e474be27a28d0b252
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-10 15:30:46 +0300

    refactor: replace default rateLimiter with customRateLimiter on login route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import * as userControllers from '../controllers/userController';
-import { rateLimiter } from '../middleware/rateLimiter'
+import { customRateLimiter } from '../middleware/rateLimiter'
 import { authMiddleware } from '../middleware/authMiddleware';
 import { roleCheck } from '../middleware/roleCheck';
 
 const router = Router();
 
 router.post('/', userControllers.registerUser);
-router.post('/login', rateLimiter, userControllers.login);
+router.post('/login', customRateLimiter, userControllers.login);
 router.post('/logout', authMiddleware, userControllers.logout);
 router.get("/me", authMiddleware, roleCheck("admin"), userControllers.getMe);
 
