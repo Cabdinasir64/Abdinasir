eeb4d1692221535a7dee3360e4a17f5a6586aa49
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-10 14:51:28 +0300

    feat: add logout route and apply rate limiting and authentication middleware to user routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import * as userControllers from '../controllers/userController';
+import { rateLimiter } from '../middleware/rateLimiter'
+import { authMiddleware } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.post('/', userControllers.registerUser);
-router.post('/login', userControllers.login);
+router.post('/login', rateLimiter, userControllers.login);
+router.post('/logout', authMiddleware, userControllers.logout);
 
 export default router;
