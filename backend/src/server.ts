e0fb3470dd23bd06f878c984218a3b05c05c4576
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-10 19:20:55 +0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import ipRoutes from "./routes/ipRoutes";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const allowedOrigins = process.env.FRONTEND_URLS?.split(',') || [];
 const app = express();
 
 
@@ -15,7 +16,13 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://abdinasir-developer.vercel.app',
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 
