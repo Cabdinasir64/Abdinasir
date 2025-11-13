788085f75b4cb7211930aa393b06f4c94c9095a1
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-13 17:11:07 +0300

    refactor: move entire admin folder to new location

--- frontend/app/admin/layout.tsx
+++
@@ -1,5 +0,0 @@
-import ClientLayout from "../components/admin/layout/Layout";
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-    return <ClientLayout>{children}</ClientLayout>;
-}
