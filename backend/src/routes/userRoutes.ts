a46b57cdc2d94d7c6cecc376694137fe5418e828
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-15 16:38:57 +0300

    feat: dynamic folder support for user profile image uploads

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,17 +1,23 @@
 import { Router } from 'express';
 import * as userControllers from '../controllers/userController';
-import { rateLimiter } from '../middleware/rateLimiter'
+import { rateLimiter } from '../middleware/rateLimiter';
 import { authMiddleware } from '../middleware/authMiddleware';
 import { roleCheck } from '../middleware/roleCheck';
-import { upload } from '../middleware/upload';
+import { uploadDynamic } from '../middleware/uploadImage';
 
 const router = Router();
+const upload = uploadDynamic("profile_images");
 
 router.post('/', userControllers.registerUser);
 router.post('/login', rateLimiter, userControllers.login);
 router.post('/logout', authMiddleware, userControllers.logout);
 router.get("/me", authMiddleware, roleCheck("admin"), userControllers.getMe);
-router.post('/profile-image', authMiddleware, upload.single('image'), userControllers.uploadProfileImage);
 
+router.post(
+    '/profile-image',
+    authMiddleware,
+    upload.single('image'),
+    userControllers.uploadProfileImage
+);
 
 export default router;
