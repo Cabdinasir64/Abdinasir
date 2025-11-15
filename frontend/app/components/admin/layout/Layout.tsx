d98eeceffa43ed141ec50021023e564791793598
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-15 15:23:21 +0300

    update UI

--- frontend/app/components/admin/layout/Layout.tsx
+++ frontend/app/components/admin/layout/Layout.tsx
@@ -65,7 +65,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Sidebar />
             <div className="flex-1 flex flex-col md:ml-64">
                 <Header />
-                <main className="flex-1 p-4 md:p-6">{children}</main>
+                <main className="flex-1 p-1 md:p-6">{children}</main>
             </div>
             <Toaster
                 position="top-right"
