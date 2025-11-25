f43bdb2fc68e4f07829f5299070196db735b4c41
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-25 11:55:30 +0300

    Update UI

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -1,11 +1,27 @@
-import Link from 'next/link'
- 
-export default function NotFound() {
-  return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
-  )
+import NotFound from '@/components/notfound/notfound'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '404 - Page Not Found | Abdinasir Dev',
+  description: 'The page you are looking for could not be found. It might have been moved or does not exist.',
+  robots: 'noindex, nofollow',
+  openGraph: {
+    title: '404 - Page Not Found | Abdinasir Dev',
+    description: 'The page you are looking for could not be found. It might have been moved or does not exist.',
+    url: 'https://abdinasir.dev/not-found',
+    siteName: 'Abdinasir Dev',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '404 - Page Not Found | Abdinasir Dev',
+    description: 'The page you are looking for could not be found. It might have been moved or does not exist.',
+  },
 }
+
+const NotFoundPage = () => {
+  return <NotFound />
+}
+
+export default NotFoundPage
