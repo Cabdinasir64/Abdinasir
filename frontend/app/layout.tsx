81744c0107de6225f6e45abd02bc24de1dc8fb82
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-25 09:47:36 +0300

    add component languageprivder

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Toaster from "@/components/ui/Toaster"
+import LanguageProvider from '@/components/providers/LanguageProvider'
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -86,11 +87,12 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={`${poppins.variable} ${playfair.variable}`}
       >
         <Toaster />
+        <LanguageProvider />
         <main>{children}</main>
       </body>
     </html >
