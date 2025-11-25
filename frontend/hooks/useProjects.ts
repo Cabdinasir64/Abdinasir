9b18ba05539c38fe30e20f1ef306eed17f2fa022
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-25 10:19:37 +0300

    Add a custom React hook useProjects to fetch project data from the API, handle loading and error states, and return the latest 5 projects sorted by creation date

---
+++ frontend/hooks/useProjects.ts
@@ -0,0 +1,41 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Project, ProjectsResponse } from "@/types/project";
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      setLoading(true);
+      try {
+        const baseUrl = process.env.NEXT_PUBLIC_API_URL
+
+        const response = await fetch(`${baseUrl}/api/projects`);
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch projects");
+        }
+
+        const json: ProjectsResponse = await response.json();
+
+        const sortedAndLimitedProjects = json.data
+          .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+          .slice(0, 5);
+
+        setProjects(sortedAndLimitedProjects);
+
+      } catch (err: any) {
+        setError(err.message || "Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  return { projects, loading, error };
+};
