fcacda499c696d96e6cb8b9e6cb6abf43fd3627b
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-26 12:06:37 +0300

    Update Perfomance

--- frontend/components/home/hero/SocialBar.tsx
+++ frontend/components/home/hero/SocialBar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import React from "react"; 
 
 const socials = [
     {
@@ -47,4 +48,4 @@ const SocialBar = ({ delay }: { delay: number }) => {
     );
 };
 
-export default SocialBar;
+export default React.memo(SocialBar);
