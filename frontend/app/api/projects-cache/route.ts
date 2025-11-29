ad8ef78a0eeb0f7e8e0993f4b9cfdf763226fbe8
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-29 19:36:35 +0300

    Update fetch data 100 projects

--- frontend/app/api/projects-cache/route.ts
+++ frontend/app/api/projects-cache/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const revalidate = 3600; 
 
 export async function GET() {
-  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/projects`;
+  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/projects?limit=100`;
 
   const res = await fetch(apiUrl, { cache: "no-store" });
   const data = await res.json();
